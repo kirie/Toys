1270038bfca53dfb2683e811e550e1da096d4f7f
Eirik <kirie@users.noreply.github.com>
2016-05-27 11:40:35 -0700

    Update ArrayZipSum.js

--- ArrayZipSum.js
+++ ArrayZipSum.js
@@ -1,5 +1,6 @@
 // Array Zip Sum
-// Given two arrays of negative/positive integers, return a new array such that each element at index n is the sum of the two elements from the other arrays at their index n’s.
+// Given two arrays of negative/positive integers, return a new array such that each element at index n is the
+// sum of the two elements from the other arrays at their index n’s.
 //
 //
 //
