5ab07fd86c2512c4e60bc634a12381b80ef2bb02
Eirik <eiriklin@yahoo.com>
2016-05-31 17:29:51 -0700

    [Add] - JS Questions

--- ArrayZipSum.js
+++ ArrayZipSum.js
@@ -4,33 +4,32 @@
 //
 //
 //
-// 
+//
 // Test Cases Input/Output
 // xs: [ 10, 20, 30 ]
-// ys: [ 1, 2, 3 ]	
+// ys: [ 1, 2, 3 ]
 // Output: [ 11, 22, 33 ]
 
 // xs: [ 100, 1 ]
-// ys: [ 50, 5 ]	
+// ys: [ 50, 5 ]
 // Output: [ 150, 6 ]
 
 // xs: [ 10, 20, 30 ]
-// ys: [ 1, 2 ]	
+// ys: [ 1, 2 ]
 // Output: [ 11, 22 ]
 
 // xs: [ 10, 20 ]
-// ys: [ 1, 2, 3 ]	
+// ys: [ 1, 2, 3 ]
 // Output: [ 11, 22 ]
 
 function zipSum (xs, ys) {
-  //Make result array
-  var result = [];
-  
+  // Make result array
+  var result = []
   // loop using the length of the longer array
-  for(var i = 0;i<(xs.length > ys.length ? ys.length: xs.length);i++){
+  for (var i = 0; i < (xs.length > ys.length ? ys.length : xs.length); i++) {
     // push the sum of to our array
-    result.push(xs[i] + ys[i]);
+    result.push(xs[i] + ys[i])
   }
   // return result
-  return result;
+  return result
 }
