178f895bee80f64f9eec55925f22244a2bad6da4
Eirik <eiriklin@yahoo.com>
2016-06-01 17:43:16 -0700

    [Add] Completed Anagrams

---
+++ Anagrams.js
@@ -0,0 +1,36 @@
+// Given a single input string, write a function that produces all possible anagrams of a string and outputs them as an array.
+// At first, don’t worry about repeated strings. What time complexity is your solution?
+
+function allAnagrams (str) {
+  // create a results object to hold every anagram we create
+  var results = {}
+
+  // create a recursive function taking in a string to build on and all the characters in the string (str)
+  function recurse (build, options) {
+    // this is our base case if our build reaches the same length.  3 characters in the test case of 'abc'
+    if (build.length === str.length) {
+      // we push our build string into our results object.  true is irrelevant
+      results[build] = true
+
+      // return so we can go back down the stack
+      return
+    }
+
+    // loop through the possible characters
+    for (var i = 0; i < options.length; i++) {
+      // create a temporary string to build on adding 1 character at a time
+      var makersq = build + options[i]
+
+      // go down the tree(and up the stack) recursing and adding options(without the element we use)
+      recurse(makersq, options.slice(0, i) + options.slice(i + 1))
+    }
+  }
+
+  // begin our recursion with not
+  recurse('', str)
+
+  // return the keys of the object to prevent duplicates as an array
+  return Object.keys(results)
+}
+
+allAnagrams('abc') // [ "abc", "acb", "bac", "bca", "cab", "cba" ]
--- EasyJSQuestions.js
+++ EasyJSQuestions.js
@@ -1,27 +1,65 @@
 // Verify a prime number?
 
-const prime = function (num) {
-  for (var i = 1; i < num; i++) {
-  	if (num % i === 0) {
-  		return false
-  	}
+function prime (num) {
+  for (var i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false
+    }
   }
   return true
 }
 
+prime(151) // TRUE
+
 // Find all prime factors of a number?
 
+function primefactors (num) {
+  var results = []
+  for (var i = 2; i < num; i++) {
+    if (num % i === 0) {
+      results.push(i)
+    }
+  }
+  return results
+}
+
+primefactors(69) // [3, 23]
+
 // Get nth Fibonacci number?
+function fibonacci (n) {
+  var x = 0
+  var y = 1
+  var result = 1
+  if (n === 0) {
+    return 0
+  }
+  for (var i = 2; i <= n; i++) {
+    result = x + y
+    x = y
+    y = result
+  }
+  return result
+}
+
+// 0, 1, 1, 2, 3, 5
+fibonacci(6) // 8
 
 // Find the greatest common divisor of two numbers?
 
+
 // Remove duplicate members from an array?
 
+
 // merge two sorted array?
+
 
 // Swap two numbers without using a temp variable?
 
 // Reverse a string in JavaScript?
+
+function reverse (string) {
+	return string.split('').reverse().join('')
+}
 
 // How would you reverse words in a sentence?
 
