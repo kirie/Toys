514d00e85bb19c15ac429dbfe03474fca5fde746
Eirik <eiriklin@yahoo.com>
2016-06-07 17:07:18 -0700

    Add - Character Frequency

---
+++ CharFrequency.js
@@ -0,0 +1,13 @@
+// Write a function that takes as its input a string and returns an array of arrays as shown
+// below sorted in descending order by frequency and then by ascending order by character.
+
+// Test cases:
+
+// string: "aaabbc"
+// output: [ [ "a", 3 ], [ "b", 2 ], [ "c", 1 ] ]
+
+// string: "mississippi"
+// output: [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ "m", 1 ] ]
+
+// string: ""
+// output: [ ]
--- linkedlistcycle.js
+++ linkedlistcycle.js
@@ -32,3 +32,4 @@ var hasCycle = function (linkedList) {
     }
   }
 }
+
