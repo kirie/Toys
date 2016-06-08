f7b3ed40a01ece5c18d66134444f0b12409d1435
Eirik <eiriklin@yahoo.com>
2016-06-07 17:08:34 -0700

    Fix - Sorted the Character Frequency

--- CharFrequency.js
+++ CharFrequency.js
@@ -11,3 +11,45 @@
 
 // string: ""
 // output: [ ]
+
+function characterFrequency (string) {
+  var obj1 = {}
+  var result = []
+  if (string.length === 0) {
+    return []
+  }
+
+  // convert the string into object pairs with occurance value
+  for (var i = 0; i < string.length; i++) {
+    if (obj1[string.charAt(i)]) {
+      obj1[string.charAt(i)]+= 1
+    }
+    else {
+      obj1[string.charAt(i)] = 1
+    }
+  }
+
+  // take object and push key pairs to arrays in an array for sorting
+  for (var key in obj1) {
+    result.push([key, obj1[key]])
+  }
+  // use sort
+  result.sort(function (a, b) {
+    if (a[1] > b[1]) {
+      // -1 = sort first before second
+      return -1
+    }
+    else if (a[1] < b[1]) {
+      // 1 = first after second
+      return 1
+    }
+    else if (a[0] > b[0]) {
+      return 1
+    }
+    else if (a[0] < b[0]) {
+      return -1
+    }
+  })
+
+  return result
+}
