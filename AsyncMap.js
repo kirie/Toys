4b2904b6c007378196847206c4bdf862b9b4effb
Eirik <eiriklin@yahoo.com>
2016-06-10 12:56:05 -0700

    Add - AsyncMap

--- AsyncMap.js
+++ AsyncMap.js
@@ -1,16 +1,36 @@
+// Implement the function asyncMap:
 
-NOTE: This challenge requires some kind of asynchronous test implementation, and these tests are not yet functional.
+// asyncMap has two parameters, an array of asynchronous functions (tasks) and a callback.
+// Each of the tasks takes a separate callback and invokes that callback when complete.
 
-Implement the function asyncMap:
+// The callback passed to asyncMap is then performed on the results of the callbacks of the tasks.
 
-asyncMap has two parameters, an array of asynchronous functions (tasks) and a callback.
-Each of the tasks takes a separate callback and invokes that callback when complete.
+// The order of these results should be the same as the order of the tasks.
+// It is important to note that this is not the order in which the tasks return,
+// but the order in which they are passed to asyncMap.
 
-The callback passed to asyncMap is then performed on the results of the callbacks of the tasks.
+// Once all the callbacks of the tasks are returned, asyncMap should invoke the callback
+// on the results array.
 
-The order of these results should be the same as the order of the tasks.
-It is important to note that this is not the order in which the tasks return,
-but the order in which they are passed to asyncMap.
+var asyncMap = function (tasks, callback) {
+  //  create a result variable to pass into the callback last
+  var result = []
+  // count each task
+  var counter = 0
 
-Once all the callbacks of the tasks are returned, asyncMap should invoke the callback
-on the results array.
+  //  loop through the tasks array
+  for (var i = 0; i < tasks.length; i++) {
+    //  Wrap as IIFE for each iteration to keep i
+    (function (i) {
+      // for each task add the value to the results array
+      tasks[i](function (value) {
+        result[i] = value
+        counter++
+        // when the counter is equal to the number of tasks, then run the callback
+        if (counter === tasks.length) {
+          callback(result)
+        }
+      })
+    })(i)
+  }
+}
