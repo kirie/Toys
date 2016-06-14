01637c6c737696fcc1a558438b9d1850554ace11
Eirik <eiriklin@yahoo.com>
2016-06-14 12:01:58 -0700

    Add - Even Occurance

---
+++ EvenOccurance.js
@@ -0,0 +1,10 @@
+// Find the first item that occurs an even number of times in an array. Remember to handle multiple even-occurrence items and return the first one. 
+// Return null if there are no even-occurrence items.
+
+// Test
+// 
+// Input: [ 1, 3, 3, 3, 2, 4, 4, 2, 5 ]	
+// Output: 2
+// Input: [ "cat", "dog", "dig", "cat" ]	
+// Output: 'cat'
+
