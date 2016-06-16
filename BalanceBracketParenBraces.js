39702fac7617a37c55a1c727d6b6b3676be64447
Eirik <eiriklin@yahoo.com>
2016-06-15 17:54:12 -0700

    Add - Adding comments to Balance

--- BalanceBracketParenBraces.js
+++ BalanceBracketParenBraces.js
@@ -10,20 +10,25 @@ const match = {
 }
 
 function balance (string) {
+	//  create a variable to hold our stack
   let result = []
+  //  loop through the string and see if the char is an open or closed
   for (let i = 0; i < string.length; i++) {
     if (string[i] === '(' || string[i] === '[' || string[i] === '{') {
       result.push(string[i])
     }
     if (string[i] === ')' || string[i] === ']' || string[i] === '}') {
+      // if the char is a closing one, pop off the stack and check if it maches
       let popped = result.pop()
       console.log(popped)
       console.log(string[i])
+      // if the char doesn't match up with the match object then we know it's false
       if (string[i] !== match[popped]) {
         return false
       }
     }
   }
+  // else return true
   return true
 }
 
