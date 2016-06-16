ce7bf6aa84c2d8c4a3dc7c24c8c0e0113720cb9f
Eirik <eiriklin@yahoo.com>
2016-06-16 11:57:27 -0700

    Add - InsertionSort w/ Comments

--- InsertionSort.js
+++ InsertionSort.js
@@ -8,9 +8,26 @@
 // should retain their relative order. Numbers, as primitives, give us no way to check this,
 // so we’ll be sorting objects with a value field, on which they will be sorted, like so:
 
-// [{value: 10}, {value: 5, order: 1}, {value: 5, order: 2}]
+// http://visualgo.net/sorting
+// select insertion sort
 
-// A stable sort must return {value: 5, order: 1}, {value:5, order: 2} in that order.
-
-// http://visualgo.net/sorting select insertion
-
+const insertionSort = (array) => {
+	// loop through the array
+  for (let i = 0; i < array.length; i++) {
+    // set current to be the current element in the array and previous is the previous value
+    // [1,2,4,5,3,7]
+    // [0,1,2,3,4,5]
+    let current = array[i] // 4 = 3
+    let previous = i - 1 // 3
+    // while the previous is valid (greater than 0) and the current is less than the previous element of array
+    while ((previous >= 0) && (current < array[previous])) {
+      // we are going to move the elements that are greater than the current up.
+      // slowly move until current != array[previous]
+      array[previous + 1] = array[previous]
+      previous--
+    }
+    // place the current value into it's correct place
+    array[previous + 1] = current
+  }
+  return array
+}
