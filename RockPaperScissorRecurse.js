f140f464bc79b9d5340dfb7762017a86eda1f855
Eirik <eiriklin@yahoo.com>
2016-06-20 14:31:57 -0700

    Add  RPS Permutation prompt

---
+++ RockPaperScissorRecurse.js
@@ -0,0 +1,6 @@
+// Given the number of rounds in a RPS game, find all the possibilities
+// for that number of rounds
+// Input: 1, Output: ['r', 'p' ,'s']
+// Input: 2, Output: ['rr', 'rp', 'rs', 'pr', 'pp', 'ps', 'sr', 'sp', 'ss']
+// Input: 0, Output: []
+
