8264e5991c1cfe11d257a124c25084d54a075b3e
Eirik <eiriklin@yahoo.com>
2016-06-22 13:03:19 -0700

    Fix - Fixing typos

--- SubsetOf.js
+++ SubsetOf.js
@@ -1,8 +1,10 @@
-// Make a method that returns true if an arry is a subset of another array
-// Make it an Array method
+// Make an an extension Array method that returns true if an array is a subset of another array
 
 // Example:
 // [13,4,5,5] is a subset of [5,2,4,13,4,5,5,9]
+// [cat, dog] is a subset of [cat, dog, horse, snake]
+// [dog] is not a subset of [rabbit, monkey, llama]
+
 Array.prototype.isSubsetOf = function(x) {
   return this.every((currval) => {
     return x.indexOf(currval) >= 0
