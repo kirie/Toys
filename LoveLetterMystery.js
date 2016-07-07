a3bd9a5d0b7418b97456425e93e7193b83d1819e
Eirik <eiriklin@yahoo.com>
2016-07-06 19:10:00 -0700

    Add - Love Letter Myster

---
+++ LoveLetterMystery.js
@@ -0,0 +1,13 @@
+// https://www.hackerrank.com/challenges/the-love-letter-mystery
+
+function processData (input) {
+  let x = input.split('\n').splice(1)
+  x.forEach(function (y) {
+    let count = 0
+    let z = y.split('').map((char) => {return char.charCodeAt(0)})
+      while (z.length > 1) {
+        count+=Math.abs(z.shift() - z.pop())
+      }
+    console.log(count)
+  })
+}
