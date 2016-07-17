93eec82e40a8b85d9bcf0a8ecb2adbcfaaae0142
Eirik <kirie@users.noreply.github.com>
2016-07-17 16:45:15 -0700

    Update Promises.js

--- Promises.js
+++ Promises.js
@@ -1,23 +1,23 @@
 let part1 = (n) => {
   return new Promise((resolve, reject) => {
-  	if(n >= 10){
-  		resolve(n*2)
-  	}
-  	else{
-  		reject('Your number is less than 10')
-  	}
+    if (n >= 10){
+      resolve(n*2)
+    }
+    else {
+      reject('Your number is less than 10')
+    }
   })
 }
 
 let part2 = (n) => {
-	return new Promise((resolve, reject) => {
-		if(n >= 100){
-			resolve (n*100)
-		}
-		else{
-			reject('Your number less than 100')
-		}
-	})
+  return new Promise((resolve, reject) => {
+    if (n >= 100) {
+      resolve (n*100)
+    }
+    else {
+      reject('Your number less than 100')
+    }
+  })
 }
 
 part1(50)
