c1b17d9fb1c74ca8dd19808271cd569766d338d3
Eirik <eiriklin@yahoo.com>
2016-07-26 15:25:35 -0700

    Add - Mars Exploration HackerRank

---
+++ MarsExploration_HR.js
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/mars-exploration
+
+function main() {
+  var S = readLine();
+  let strarr = S.split('');
+  let options = ['S','O','S'];
+  let tripletarr = [];
+  while(strarr.length > 0){
+    tripletarr.push(strarr.splice(0, 3));
+  }
+  let final = tripletarr.map((v1) => 
+                v1.map((v2, i2) =>
+                  v2 === options[i2] ? 0 : 1)
+                .reduce((pv, cv) => pv + cv))
+                .reduce((pv2, cv2) => pv2 + cv2);
+  console.log(final);
+}
