a8e72682a87b0dd2da939259f4ce230e2971df8e
Eirik <eiriklin@yahoo.com>
2016-08-15 09:26:56 -0700

    Add - Alternate Space Commas ASKED

---
+++ AlternateSpaceCommas.js
@@ -0,0 +1,12 @@
+// Write a function that takes a string and outputs the string with alternating spaces and commas
+// inserted in between each character
+
+'use strict';
+
+function altSpaceComma (string) {
+  let result = '';
+  for(let i = 0; i < string.length ;i++) {
+    result = result.concat(i % 2 ? string[i].concat(',') : string[i].concat(' '));
+  }
+  return result;
+}
