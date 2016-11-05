7f07e28fb2f9d4e49b3cf9ebd173a493d91cf7e6
Eirik <eiriklin@yahoo.com>
2016-11-05 10:38:23 -0700

    Add - Stock Profit

--- BalanceBracketParenBraces.js
+++ BalanceBracketParenBraces.js
@@ -2,12 +2,14 @@
 // Such that:
 // '( ) [ ] { }' = true
 // '[ { ] }' = false
+'use strict';
+const expect = require('expect');
 
 const match = {
   '(': ')',
   '[': ']',
   '{': '}'
-}
+};
 
 function balance (string) {
 	//  create a variable to hold our stack
@@ -15,21 +17,32 @@ function balance (string) {
   //  loop through the string and see if the char is an open or closed
   for (let i = 0; i < string.length; i++) {
     if (string[i] === '(' || string[i] === '[' || string[i] === '{') {
-      result.push(string[i])
+      result.push(string[i]);
     }
     if (string[i] === ')' || string[i] === ']' || string[i] === '}') {
       // if the char is a closing one, pop off the stack and check if it maches
-      let popped = result.pop()
-      console.log(popped)
-      console.log(string[i])
+      let popped = result.pop();
+      console.log(popped);
+      console.log(string[i]);
       // if the char doesn't match up with the match object then we know it's false
       if (string[i] !== match[popped]) {
-        return false
+        return false;
       }
     }
   }
   // else return true
-  return true
+  return true;
 }
 
-balance('[ { ] }')
+const testBalance = () => {
+  expect(
+    balance('[ { ] }')
+  ).toEqual(false);
+
+  expect(
+    balance('{[ [] ] }')
+  ).toEqual(true);  
+};
+
+testBalance();
+console.log('All tests passed');
---
+++ StockProfit.js
@@ -0,0 +1,33 @@
+// Given an array of yesterday's stock prices where the index represents minuetes 
+// after the stock market has opened and the value is the stock price at that minuete,
+// find the greatest profit that could have been made from yesterday's stocks.
+
+'use strict';
+const expect = require('expect');
+
+const stock = (arr) => {
+
+  // initial variables
+  let minPrice = arr[0];
+  let maxProfit = 0;
+
+  arr.forEach((v) => {
+    let current = v;
+    minPrice = Math.min(minPrice, current);
+    maxProfit = Math.max(maxProfit, current - minPrice);
+  });
+
+  return maxProfit;
+};
+
+const testFunction = () => {
+  expect(
+    stock([500,300,900,400,500,9000,400,400])
+  ).toEqual(8700);
+  expect(
+    stock([200,200,200,200,200,200])
+  ).toEqual(0);
+};
+
+testFunction();
+console.log('All tests passed');
