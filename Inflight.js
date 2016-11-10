eb09c34c8cd8127341d26cc6174d65a9399c77e8
Eirik <eiriklin@yahoo.com>
2016-11-10 10:59:02 +0800

    Edit - Refactor with Inflight with .every

--- Inflight.js
+++ Inflight.js
@@ -3,20 +3,23 @@ const expect = require('expect');
 
 const movieFlight = (movie_arr, flight_length) => {
   let hash = {};
-  for(let i = 0; i < movie_arr.length; i++){
-    let remaining = flight_length - movie_arr[i];
-    if(hash[remaining]){
-      return true
+  return !movie_arr.every((v) => {
+    if(hash[flight_length - v]){
+      return false;
     }
-    hash[movie_arr[i]] = true
-  }
-  return false
+    hash[v] = true;
+    return true;
+  });
 };
+
 
 const testFunction = () => {
   expect(
     movieFlight([90,120,105,50,163,140,110,85], 155)
   ).toEqual(true);
+  expect(
+    movieFlight([90,120,105,51,163,140,110,85], 155)
+  ).toEqual(false);
 };
 
 testFunction();
