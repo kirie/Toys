c93a49d26b3481f800f528a93be62cd84797d600
Eirik <eiriklin@yahoo.com>
2016-11-30 06:40:57 -0800

    Edit - Merge conflict fix

--- Fibonacci.js
+++ Fibonacci.js
@@ -2,29 +2,30 @@
 // 0 1 1 2 3 5 8 13...
 
 const fibonacci = (n) => {
-  if(n === 0) {
-    return 0
-	}
-  let x = 0
-  let y = 1
-  let addition = 1
-  for (let i = 2; i <= n; i++) {
-    addition = x + y
-    x = y
-    y = addition
+  if (n === 0) {
+    return 0;
   }
-  return addition
-}
+  let x = 0;
+  let y = 1;
+  let addition = 1;
+  for (let i = 2; i <= n; i++) {
+    addition = x + y;
+    x = y;
+    y = addition;
+  }
+  return addition;
+};
 
 const fibonacciRecursive = (n) => {
   if (n === 0) {
-    return 0
+    return 0;
   }
-  if (n <= 2){
-    return 1
+  if (n <= 2) {
+    return 1;
   }
   else {
-    return fibonacciRecursive(n-1) + fibonacciRecursive(n-2)
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
   }
-}
+};
 
+module.exports = fibonacciRecursive;
--- Memoize.js
+++ Memoize.js
@@ -1,56 +1,30 @@
-<<<<<<< bb5f13afc4347f2cc76b91431cba5046e4c95d2e:Memoize.js
 // Create a memoize function that accepts a function and returns
-// values that have been seen before.
-=======
-// Given a string and an index of an opening parenthesis,
-// find the index of the closing parenthesis
-
-// Constraint: O(n) time and O(1) space
+// values that have been calculated before.
 
 const expect = require('expect');
->>>>>>> Add - Parenthesis Index:empty.js
+const Fibonacci = require('./Fibonacci');
 
-function parenthesisIndex(str, startIndex) {
-  let count = 0;
-  for (let i = startIndex; i < str.length; i++) {
-    if (str[i] === '(') {
-      count++;
+function memoize(func) {
+  const called = {};
+  return function (item) {
+    if (item in called) {
+      return called[item];
     }
-    if (str[i] === ')') {
-      if (count <= 1) {
-        return i;
-      }
-      count--;
-    }
-  }
-  return 'An Error Occured';
+    called[item] = func.apply(this, arguments);
+    return called[item];
+  };
 }
 
 
 // Test
 
-const testFunction = () => {
-  expect(
-    parenthesisIndex('Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing.', 10)
-  ).toEqual(79);
-};
 
-<<<<<<< bb5f13afc4347f2cc76b91431cba5046e4c95d2e:Memoize.js
-function fibonacci(num) {
-  if (num < 2) return 1;
-  else return fibonacci(num - 1) + fibonacci (num - 2);
-}
+const testFib = memoize(Fibonacci);
 
-const testFib = memoize(fibonacci);
-
-console.time('First run');
+console.time('first run');
 testFib(30);
-console.timeEnd('First run');
+console.timeEnd('first run');
 
-console.time('Memoized call');
+console.time('memoized call');
 testFib(30);
-console.timeEnd('Memoized call');
-=======
-testFunction();
-console.log('All tests passed');
->>>>>>> Add - Parenthesis Index:empty.js
+console.timeEnd('memoized call');
