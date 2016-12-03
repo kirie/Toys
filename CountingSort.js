ed15c039b44dbbc64295575a2532e5e966699b59
Eirik <eiriklin@yahoo.com>
2016-12-02 17:19:19 -0800

    Add - Prompt and test for Counting

--- CountingSort.js
+++ CountingSort.js
@@ -1,3 +1,9 @@
+// Given an array of integers between 0 and a maximum, sort
+// all the numbers with a constraint of O(n)
+
+// Constraint: O(n)
+
+const expect = require('expect');
 
 function sortScores(arr, max) {
   const scores = new Array(max + 1);
@@ -16,3 +22,16 @@ function sortScores(arr, max) {
   });
   return sorted;
 }
+
+
+// Test
+
+
+const testFunction = () => {
+  expect(
+    sortScores([20, 5, 30, 40, 10, 99, 30, 80], 100)
+  ).toEqual([5, 10, 20, 30, 30, 40, 80, 99]);
+};
+
+testFunction();
+console.log('All tests passed');
