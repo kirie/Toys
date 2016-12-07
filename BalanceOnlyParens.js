3152b1ffeeeb91b425e060af96016901bb2ffc89
Eirik <eiriklin@yahoo.com>
2016-12-07 15:40:37 -0800

    Add - Balance only Parens

---
+++ BalanceOnlyParens.js
@@ -0,0 +1,32 @@
+// Write a function that takes an input string and determines if the 
+// parenthesis are balanced.
+
+const expect = require('expect');
+
+function balanceOnlyParens(str) {
+  return !str.split('').reduce((p, v) => {
+    if (p < 0) {
+      return p;
+    }
+    if (v === '(') {
+      return ++p;
+    }
+    if (v === ')') {
+      return --p;
+    }
+    return p;
+  }, 0);
+}
+
+const testBalanceParens = () => {
+  expect(
+    balanceOnlyParens('(())')
+  ).toEqual(true);
+
+  expect(
+    balanceOnlyParens(')(')
+  ).toEqual(false);
+};
+
+testBalanceParens();
+console.log('All tests passed');
