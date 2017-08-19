7a2d157ce93a8407363adb6f1897f881e2da3344
Eirik <eiriklin@yahoo.com>
2017-08-19 14:16:07 -0700

    Add - AsyncAwait

--- es7_AsyncAwait.js
+++ es7_AsyncAwait.js
@@ -1,11 +1,15 @@
 // Async/Await
+const fetch = require('isomorphic-fetch')
+const url = 'https://reqres.in/api/users?page=1'
 
-async function getSomething () {
-  const response = await fetch('https://api.somewhere.com')
+async function getSomething (testUrl) {
+  const response = await fetch(testUrl)
   const response2 = await response.json()
   return response2
 }
 
-const getSomethingOneLine = async () =>
-  await (await fetch('https://api.somesite.com')).json()
+const getSomethingOneLine = async (testUrl) =>
+  await (await fetch(testUrl)).json()
 
+getSomething(url).then(result => console.log(result))
+getSomethingOneLine(url).then(result => console.log(result))
